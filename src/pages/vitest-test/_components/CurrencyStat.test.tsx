4987ee5cb54270a6ba39ae0914e992b40e174ce4
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-11-16 14:38:23 +0100

    fix: move test

--- src/pages/vitest-test/_components/CurrencyStat.test.tsx
+++ src/pages/vitest-test/_components/CurrencyStat.test.tsx
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/react";
-import CurrencyStat from "../src/pages/vitest-test/_components/CurrencyStat.tsx";
+import CurrencyStat from "./CurrencyStat.tsx";
 
 it("displays the gives value formatted as currency", () => {
   const { getByText } = render(<CurrencyStat value={123} />);
